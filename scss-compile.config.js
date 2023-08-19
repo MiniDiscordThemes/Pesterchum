d60e1d4ae45950ae8740da9d175bfcb7fc9d1ab0
Saltssaumure <themes@saltssaumure.net>
2023-08-19 22:55:50 +0100

    Add info files

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Pesterchum" /* The official name of your theme. */,
+        scss: "chum" /* The name of your base scss file. */,
+        repo: "Pesterchum" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
